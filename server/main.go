33fae0e38af5cdffb7d510774f3ec195faa9410b
Hailong Zhao <hailong.zhao@sycle.net>
2021-11-15 11:06:08 -0500

    Iteration 2

--- server/main.go
+++ server/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"strings"
 )
 
 func logFatal(err error) {
@@ -59,12 +62,62 @@ func broadcastMessage(conn net.Conn) {
 			break
 		}
 
-		for item := range openConnections {
-			if item != conn {
-				item.Write([]byte(message))
+		if strings.HasPrefix(message, "GET") {
+			response := handleHttpRequest(message)
+
+			conn.Write(response)
+
+			break
+		} else {
+			for item := range openConnections {
+				if item != conn {
+					item.Write([]byte(message))
+				} else {
+					item.Write([]byte(fmt.Sprint("Received: ", message)))
+				}
 			}
 		}
 	}
 
 	deadConnection <- conn
+}
+
+var httpMessageTemplate = `HTTP/1.1 %d OK
+Cache-Control: no-cache, private
+Content-Type: %s
+Content-Length: %d
+
+`
+
+func handleHttpRequest(message string) []byte {
+	statusCode := 200
+	contentType := "text/html"
+
+	var (
+		content []byte
+		err     error
+	)
+
+	switch {
+	case strings.HasPrefix(message, "GET / HTTP"):
+		content, err = os.ReadFile("./wwwroot/index.html")
+		logFatal(err)
+	case strings.HasPrefix(message, "GET /favicon.ico HTTP"):
+		contentType = "image/vnd.microsoft.icon"
+		content, err = os.ReadFile("./wwwroot/favicon.ico")
+		logFatal(err)
+	default:
+		statusCode = 404
+		content, err = os.ReadFile("./wwwroot/404.html")
+		logFatal(err)
+	}
+
+	header := []byte(fmt.Sprintf(
+		httpMessageTemplate,
+		statusCode,
+		contentType,
+		len(content),
+	))
+
+	return append(header, content...)
 }
